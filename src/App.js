8d41b44a6e143622b92c338b8154b0c58b7ce1c8
oleksandr.dubik <oleksandr.skinkeen@gmail.com>
2020-06-18 14:28:25 +0300

    add modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
-import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
+// import Modal from './Modal/Modal'
+
+const AddTodo = React.lazy(() => import('./Todo/AddTodo'))
+
+// const AddTodo = React.lazy(() => new Promise(resolve =>{
+//   setTimeout(()=>{
+//     resolve(import('./Todo/AddTodo'))
+//   },3000)
+// }))
 
 function App() {
-  let [todos, setTodos] = React.useState([
-    { id: 1, completed: true, title: "Buy bread" },
-    { id: 2, completed: false, title: "Buy cheeze" },
-    { id: 3, completed: false, title: "Buy milk" },
-  ]);
+  let [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState([true]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => {
+          setTodos(todos);
+          setLoading(false)
+        // setTimeout(() => {
+        //   setTodos(todos);
+        //   setLoading(false)
+        // }, 2000);
+      });
+  }, []);
 
   function toggleTodo(id) {
     setTodos(
@@ -32,7 +51,7 @@ function App() {
         {
           title,
           id: Date.now(),
-          completed: false
+          completed: false,
         },
       ])
     );
@@ -42,10 +61,15 @@ function App() {
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>React tutorial</h1>
-        <AddTodo onCreate={addTodo} />
+        {/* <Modal /> */}
+        <React.Suspense fallback={<p>Loading</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
+
+        {loading && <Loader />}
         {todos.length ? (
           <TodoList onToggle={toggleTodo} todos={todos} />
-        ) : (
+        ) : loading ? null : (
           <p>No todos</p>
         )}
       </div>
